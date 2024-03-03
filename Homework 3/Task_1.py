83408acd335c0fa52c790b9c030fbe2e308f5a68
alvara198 <161085644+alvara198@users.noreply.github.com>
2024-03-03 23:22:06 +0400

    Add files via upload

---
+++ Homework 3/Task_1.py
@@ -0,0 +1,8 @@
+import math
+
+First_Number = float(input("შეიყვანეთ პირველი რიცხვი: "))
+Second_Number = float(input("შეიყვანეთ მეორე რიცხვი: "))
+
+Result = math.pow(First_Number, Second_Number)
+
+print(Result)
